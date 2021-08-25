5787ec77ed2b0b58add95d1fbf74b578ac59387a
khachiya <k.hachiya@gmail.com>
2021-08-25 14:28:12 +0900

    Add files via upload

---
+++ jp/answers/ex08/csv.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile('sales.csv','utf8',function(err, data) {
+	let dataArray = data.split(/\r?\n/);
+	let sum=0;
+
+	for (let i=1; i<dataArray.length; i++) {
+		dataArray[i]=dataArray[i].split(',');
+		sum += dataArray[i][0] * dataArray[i][1];
+	}
+
+	console.log(sum);
+
+});
